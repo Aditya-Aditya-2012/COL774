f670b34751f79ddae8c7b1fac5990d1d76735ed7
Aditya-Aditya-2012 <aditya20rajan@gamil.com>
2024-08-25 02:18:36 +0530

    part a done mkcccc

--- Assignment1.2/logistic.py
+++ Assignment1.2/logistic.py
@@ -1,128 +1,79 @@
+#Assignment 1.2 part a
 import numpy as np
 import pandas as pd
 from scipy.special import softmax
 from scipy.sparse import csr_matrix
 import math
 import sys
+from train_algos import *
+##################################################
+##################### DATA #######################
+##################################################
+def dataloader(train_path, params_path):
+    train = pd.read_csv(train_path)
+    x = train.iloc[:, :-1]
+    y = train.iloc[:, -1]
 
-def preprocess(train):
-    y_train = np.array(train['Race'])
-    train = train.drop(columns=['Race'])
-
-    X_train = train.to_numpy()
-    X_train = np.c_[np.ones(X_train.shape[0]), X_train]  
+    X = x.to_numpy(dtype=np.float64)
+    ones_column = np.ones((X.shape[0], 1), dtype=np.float64)
     
-    y_encod = pd.get_dummies(y_train).to_numpy()
-    
-    return X_train, y_encod
+    X = np.hstack((ones_column, X))
+    #shape X=(n,m+1)
 
-# Computes the modified log-likelihood loss for weighted logistic regression.
-def compute_loss(X, y, W, freq):
-    n = X.shape[0]
-    loss = 0
-    for i in range(n):
-        sumj = np.sum(np.exp(X[i].dot(W)))
-        for j in range(W.shape[1]):
-            if y[i, j] == 1:
-                loss += (X[i].dot(W[:, j]) - max_val - np.log(sumj)) / freq[j]
-    return -loss / (2 * n)
+    # one hot encode y as given in the writeup
+    y = y.to_numpy(dtype=np.float64)
+    Y = pd.get_dummies(y).to_numpy(dtype=np.float64)
+    # Y = Y.astype(float64)
+    #done, y of shape (n,4)
 
-# Computes the gradient of the loss with respect to the weights.
-def compute_gradient(X, y, W, freq):
-    n, m = X.shape
-    k = W.shape[1]
-    gradient = np.zeros(W.shape)
-
-    for i in range(n):
-        exp_values = np.exp(X[i].dot(W))
-        sumj = np.sum(exp_values)
-        for j in range(k):
-            indicator = y[i, j]
-            probs = exp_values[j] / sumj
-            gradient[:, j] += (indicator - probs) * X[i] / freq[j]
-    
-    return -gradient / (2 * n)
-
-# Performs exact line search using ternary search to find the optimal learning rate.
-def ternary_search(X, y, W, freq, g, eta0):
-    eta_l = 0
-    eta_h = eta0
-
-    while compute_loss(X, y, W - eta_h * g, freq) >= compute_loss(X, y, W, freq):
-        eta_h *= 2
-
-    for _ in range(20):
-        eta1 = (2 * eta_l + eta_h) / 3
-        eta2 = (eta_l + 2 * eta_h) / 3
-
-        loss1 = compute_loss(X, y, W - eta1 * g, freq)
-        loss2 = compute_loss(X, y, W - eta2 * g, freq)
-
-        if loss1 > loss2:
-            eta_l = eta1
-        elif loss1 < loss2:
-            eta_h = eta2
-        else:
-            eta_l = eta1
-            eta_h = eta2
-
-    return (eta_l + eta_h) / 2
-
-
-# Executes mini-batch gradient descent with exact line search to update weights.
-def mini_batch_gradient_descent(X, y, W, freq, batch_size, epochs, eta0, train_strat, k):
-    m, n = X.shape
-    for epoch in range(epochs):
-        for i in range(0, m, batch_size):
-            X_batch = X[i:i + batch_size]
-            y_batch = y[i:i + batch_size]
-            
-            g = compute_gradient(X_batch, y_batch, W, freq)
-            if train_strat == 1:
-                W -= eta0 * g
-            elif train_strat == 2:
-                eta = eta0 / (1 + k * i)
-                W -= eta * g
-            else:
-                eta = ternary_search(X_batch, y_batch, W, freq, g, eta0)
-                W -= eta * g
-    return W
-
-def main(task, train_file, params_file, output_file):
-    train_data = pd.read_csv(train_file)
-
-    X_train, y_train = preprocess(train_data)
-
-    if task == 'a':
-        train_strat = 1
-        k = 0
-        eta0 = 0
-        epochs = 0
-        batch_size = 0
-        
-        with open(params_file, 'r') as f:
+    #now, load params
+    k=0
+    lr=0
+    with open(params_path, 'r') as f:
             params = f.read().splitlines()
-            train_strat = int(params[0])
-            if train_strat == 1:
-                eta0 = float(params[1])
-            elif train_strat == 2:
-                eta0, k = map(float, params[1].strip().split(','))
-            else:
-                eta0 = float(params[1])
-            epochs = int(params[2])
-            batch_size = int(params[3])
+    train_strat = int(params[0])
+    if train_strat == 2 :
+        lr, k = map(float, params[1].strip().split(','))
+    else:
+        lr = float(params[1])
+    epochs = int(params[2])
+    batch_size = int(params[3])
 
-        m = X_train.shape[1]
-        n = y_train.shape[1]
-        W = np.zeros((m, n), dtype=np.float64)
+    return X, Y, train_strat, lr, k, epochs, batch_size
+
+##################################################
+##################### part a #####################
+##################################################
+
+task = sys.argv[1]
+train_path = sys.argv[2]
+params_path = sys.argv[3]
+output_file = sys.argv[4]
+
+X, Y, train_strat, lr, k, epochs, batch_size = dataloader(train_path, params_path)
+
+if task == 'a' :
+    m = X.shape[1]
+    classes = Y.shape[1]
+
+    W = np.zeros((m, classes), dtype=np.float64)
+    freq = np.array([np.sum(Y[:, j]) for j in range(classes)], dtype=np.float64)
         
-        freq = np.array([np.sum(y_train[:, j]) for j in range(n)], dtype=np.float64)
-        W = mini_batch_gradient_descent(X_train, y_train, W, freq, batch_size, epochs, eta0, train_strat, k)
-        np.savetxt(output_file, W)
+    if train_strat == 1 :
+        W = constant_lr(X, Y, W, lr, epochs, batch_size, freq)
+    
+    elif train_strat == 2 :
+        W = adaptive_lr(X, Y, W, lr, k, epochs, batch_size, freq)
+    
+    else :
+        W = ternary_lr(X, Y, W, lr, epochs, batch_size, freq)
 
-if __name__ == "__main__":
-    task = sys.argv[1]
-    train_file = sys.argv[2]
-    params_file = sys.argv[3]
-    output_file = sys.argv[4]
-    main(task, train_file, params_file, output_file)
+    np.savetxt(output_file, W)
+
+##################################################
+##################### part b #####################
+##################################################
+        
+
+
+    
---
+++ Assignment1.2/train_algos.py
@@ -0,0 +1,123 @@
+# This file contains all the three training algos to be implemented
+
+import numpy as np
+import scipy
+import math
+from scipy.special import softmax
+
+# loss = -1/2n*(sumi(sumj({yi=j}log(g_wj(xi)))))
+# g_wj(x)=exp(wj.T.x)/sumk(exp(wi.T.x))
+
+def softmax(logits):
+    exp_logits = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+    return np.float64(exp_logits / np.sum(exp_logits, axis=1, keepdims=True))
+
+
+def loss_fn(X, y, w, freq):
+    n, d = X.shape
+    k = w.shape[1]
+
+    # Compute linear outputs
+    logits = np.dot(X, w)  # Shape (n, k)
+
+    # Apply softmax to get the predicted probabilities
+    g_wj_x = softmax(logits)  # Shape (n, k)
+
+    # Initialize the loss
+    loss = 0
+
+    # Compute the loss for each class
+    for j in range(k):
+        # Select the corresponding softmax output for class j where y is 1 for class j
+        selected_probs = g_wj_x[:, j][y[:, j] == 1]  # Shape (number of samples where y == j,)
+        
+        # Compute the weighted sum of log probabilities
+        loss += -np.sum(np.log(selected_probs) / freq[j])
+    
+    # Average the loss and return
+    loss = loss / (2 * n)
+    
+    return loss
+    
+
+def gradient(X, Y, W, freq) :
+    #grad = 1/2n(X.T.(U-Y))
+    U=softmax(np.dot(X,W))
+    G = np.zeros_like(U, dtype=np.float64)
+    n = np.float64(Y.shape[0])
+
+    for i in range(Y.shape[0]) :
+        index = np.where( Y[i] == 1 )[0][0]
+        fact = np.float64(2)*n*freq[index]
+        G[i] = (U[i] - Y[i]) / fact
+
+    grad = np.transpose(X)@G
+
+    return grad
+
+def constant_lr(X, Y, W, lr, epochs, batch_size, freq) :
+    n_batches = Y.shape[0] // batch_size
+
+    for epoch in range(epochs) :
+        loss = 0
+        for i in range(n_batches) :
+            X_batch = np.float64(X[i*batch_size : (i+1)*batch_size])
+            Y_batch = np.float64(Y[i*batch_size : (i+1)*batch_size])
+
+            W -=  np.float64(lr)*gradient(X_batch, Y_batch, W, freq)
+            loss += loss_fn(X_batch, Y_batch, W, freq)
+
+        print(f"epoch : {epoch} loss : {np.mean(loss)}")
+    return W
+
+def adaptive_lr(X, Y, W, lr, k, epochs, batch_size, freq) :
+    n_batches = Y.shape[0] // batch_size
+
+    for epoch in range(epochs) :
+        for i in range(n_batches) :
+            X_batch = np.float64(X[i*batch_size : (i+1)*batch_size])
+            Y_batch = np.float64(Y[i*batch_size : (i+1)*batch_size])
+
+            W -=  np.float64(lr/(1 + k * (epoch+1)))*gradient(X_batch, Y_batch, W, freq)
+    
+    return W
+
+def ternary_search(X, y, W, freq, g, eta0) :
+    eta_l = 0
+    eta_h = eta0
+
+    while compute_loss(X, y, W - eta_h * g, freq) >= compute_loss(X, y, W, freq):
+        eta_h *= 2
+
+    for _ in range(20):
+        eta1 = (2 * eta_l + eta_h) / 3
+        eta2 = (eta_l + 2 * eta_h) / 3
+
+        loss1 = loss(X, y, W - eta1 * g, freq)
+        loss2 = loss(X, y, W - eta2 * g, freq)
+
+        if loss1 > loss2:
+            eta_l = eta1
+        elif loss1 < loss2:
+            eta_h = eta2
+        else:
+            eta_l = eta1
+            eta_h = eta2
+
+    return (eta_l + eta_h) / 2
+
+def ternary_lr(X, Y, W, lr, epochs, batch_size, freq) :
+    n_batches = Y.shape[0] // batch_size
+
+    for epoch in range(epochs) :
+        for i in range(n_batches) :
+            X_batch = np.float64(X[i*batch_size : (i+1)*batch_size])
+            Y_batch = np.float64(Y[i*batch_size : (i+1)*batch_size])
+            
+            g = gradient(X_batch, Y_batch, W, freq)
+
+            lr = ternary_search(X_batch, y_batch, W, freq, g, lr)
+
+            W -=  np.float64(lr)*gradient(X_batch, Y_batch, W, freq)
+    
+    return W
