6c9760839681db39ccf494e0162b1838afd1b5e8
Harshul Malik <97736368+Harshul3010@users.noreply.github.com>
2024-10-05 15:37:27 +0530

    Wide Resnet with Temperature Scaling

--- Assignment3/train.py
+++ Assignment3/train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Subset, TensorDataset
 import torchvision.transforms as transforms
@@ -9,6 +10,8 @@ import sys
 import pickle
 
 torch.manual_seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def load_data(file_path):
     print(f"Loading data from {file_path}...")
@@ -106,8 +109,8 @@ def calculate_accuracy(model, data_loader):
     total = 0
     with torch.no_grad():  # Disable gradient computation during inference
         for inputs, labels in data_loader:
-            inputs = inputs.float()
-            labels = labels.long()
+            inputs = inputs.float().to(device)
+            labels = labels.long().to(device)
             outputs = model(inputs)
             _, predicted = torch.max(outputs, 1)  # Get the index of the max log-probability
             total += labels.size(0)
@@ -116,14 +119,131 @@ def calculate_accuracy(model, data_loader):
     accuracy = 100 * correct / total
     return accuracy
 
-def train_model(model, train_loader, save_weights_path):
-    model = model.float()  
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3)
 
+class ModelWithTemperature(nn.Module):
+    """
+    A thin decorator, which wraps a model with temperature scaling
+    model (nn.Module):
+        A classification neural network
+        NB: Output of the neural network should be the classification logits,
+            NOT the softmax (or log softmax)!
+    """
+    def __init__(self, model):
+        super(ModelWithTemperature, self).__init__()
+        self.model = model
+        self.temperature = nn.Parameter(torch.ones(1) * 1.5)
+
+    def forward(self, input):
+        logits = self.model(input)
+        return self.temperature_scale(logits)
+
+    def temperature_scale(self, logits):
+        """
+        Perform temperature scaling on logits
+        """
+        # Expand temperature to match the size of logits
+        temperature = self.temperature.unsqueeze(1).expand(logits.size(0), logits.size(1))
+        return logits / temperature
+
+    # This function probably should live outside of this class, but whatever
+    def set_temperature(self, valid_loader):
+        """
+        Tune the tempearature of the model (using the validation set).
+        We're going to set it to optimize NLL.
+        valid_loader (DataLoader): validation set loader
+        """
+        self.cuda()
+        nll_criterion = nn.CrossEntropyLoss().cuda()
+        ece_criterion = _ECELoss().cuda()
+
+        # First: collect all the logits and labels for the validation set
+        logits_list = []
+        labels_list = []
+        with torch.no_grad():
+            for input, label in valid_loader:
+                input = input.cuda()
+                logits = self.model(input)
+                logits_list.append(logits)
+                labels_list.append(label)
+            logits = torch.cat(logits_list).cuda()
+            labels = torch.cat(labels_list).cuda()
+
+        # Calculate NLL and ECE before temperature scaling
+        before_temperature_nll = nll_criterion(logits, labels).item()
+        before_temperature_ece = ece_criterion(logits, labels).item()
+        print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
+
+        # Next: optimize the temperature w.r.t. NLL
+        optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
+
+        def eval():
+            optimizer.zero_grad()
+            loss = nll_criterion(self.temperature_scale(logits), labels)
+            loss.backward()
+            return loss
+        optimizer.step(eval)
+
+        # Calculate NLL and ECE after temperature scaling
+        after_temperature_nll = nll_criterion(self.temperature_scale(logits), labels).item()
+        after_temperature_ece = ece_criterion(self.temperature_scale(logits), labels).item()
+        print('Optimal temperature: %.3f' % self.temperature.item())
+        print('After temperature - NLL: %.3f, ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+
+        return self
+
+
+class _ECELoss(nn.Module):
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
+
+    The input to this loss is the logits of a model, NOT the softmax scores.
+
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
+
+def train_model(model, train_loader, save_weights_path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    model = model.float().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3)
     
     # Simple validation split (80% train, 20% validation)
     dataset = train_loader.dataset
@@ -139,7 +259,7 @@ def train_model(model, train_loader, save_weights_path):
     train_loader = DataLoader(train_sampler, batch_size=train_loader.batch_size, shuffle=True)
     val_loader = DataLoader(val_sampler, batch_size=train_loader.batch_size, shuffle=False)
     
-    training_duration = 5 * 60 * 60  
+    training_duration = 2.5 * 60 * 60  
     start_time = time.time()
 
     best_val_accuracy = 0
@@ -175,7 +295,6 @@ def train_model(model, train_loader, save_weights_path):
         
         # Evaluate on validation set
         val_accuracy = calculate_accuracy(model, val_loader)
-        
         elapsed_time = time.time() - start_time
         print(f"Epoch {epoch}, Time: {elapsed_time:.2f}s, Loss: {epoch_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%, Val Accuracy: {val_accuracy:.2f}%")
         
@@ -188,10 +307,15 @@ def train_model(model, train_loader, save_weights_path):
 
 
     model.load_state_dict(torch.load(save_weights_path))
-    final_val_accuracy = calculate_accuracy(model, val_loader)
-    print(f"Final Validation accuracy for the model: {final_val_accuracy:.2f}%")
+
+    scaled_model = ModelWithTemperature(model)
+    scaled_model.set_temperature(val_loader)
     
-    return model, final_val_accuracy
+    final_val_accuracy = calculate_accuracy(scaled_model, val_loader)
+
+    print(f"Final Validation accuracy for the calibrated model: {final_val_accuracy:.2f}%")
+    
+    return scaled_model, final_val_accuracy
 
 def main():
     if len(sys.argv) != 4:
@@ -216,7 +340,7 @@ def main():
     
     print("Creating train dataset and dataloader...")
     train_dataset = TensorDataset(normalized_images, labels)
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
     print("Data preparation completed.")
     
     print("Initializing the model...")
@@ -226,8 +350,6 @@ def main():
     print("Starting model training...")
     save_weights_path = 'best_model.pth'
     model, final_val_accuracy = train_model(model, train_loader, save_weights_path)
-
-    print(final_val_accuracy)
     
     print("Training completed. Saving the model...")
     torch.save(model.state_dict(), 'model.pth')
